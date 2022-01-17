1a3c508c92a5875ada83f54f1a71dda65c9945e8
MiyabaraYouta <miyayou0527@gmail.com>
2022-01-17 17:48:18 +0900

    回避の座標移動追加

--- UnityProject/Assets/Player/PlayerScript/Player_Controll.cs
+++ UnityProject/Assets/Player/PlayerScript/Player_Controll.cs
@@ -7,6 +7,9 @@ using UnityEngine.SceneManagement;
 
 public class Player_Controll : MonoBehaviour
 {
+    [SerializeField, Tooltip("RigidBody")]
+    private Rigidbody _RigidBody = null;
+
     [SerializeField]
     float PlayerWlakSpeed;
     [SerializeField]
@@ -15,33 +18,80 @@ public class Player_Controll : MonoBehaviour
     [SerializeField]
     public int PlayerAttackPoint;
 
+    [SerializeField, Tooltip("武器のオブジェクト")]
+    private GameObject _SwordObject = null;
+
+    [SerializeField, Tooltip("連続攻撃の猶予時間")]
+    private float _CombAttackGraceTime = 1.0f;
+
+    [SerializeField, Tooltip("攻撃パターンの種類")]
+    private int _AttackPatternNum = 3;
+
+    [SerializeField, Tooltip("回避の大きさ")]
+    private float _AvoidanceValue = 50.0f;
 
     private Vector3 latestPosition;
     private Animator PlayerAttackAnimator;
+    private Collider _SwordCollider;
 
-    //-----------------------------------------------
-    //攻撃コライダー追加（ジョ）
-    [SerializeField]private Collider attackArea;
-    //-----------------------------------------------
+    
+    private int _AttackType = 0;
+    private float _NextActionTime;
+
+
+    private bool _IsGaraceTime = false;
+    private bool _IsAvoid = false;
+
+    public float SmoothTime = 2f;
+    public float Speed = 1f;
+    public float JourneyLength = 10f;
+    private float _StartTime = 0;
+
+    private Vector3 AvoidPos_Start = new Vector3();
+    private Vector3 AvoidPos_End = new Vector3();
 
     private void Awake()
     {//スタート関数前に何か初期化する時用
     }
     void Start()
     {
+        //animtion
         PlayerAttackAnimator = GetComponent<Animator>();
+        PlayerAttackAnimator.SetFloat("AttackSpeed", 1);
+
         latestPosition = transform.position;
         PlayerAttackPoint = 10;
+        _AttackType = 0;
+        _StartTime = Time.time;
+
+        //武器の当たり判定の設定
+        _SwordCollider = _SwordObject.GetComponent<BoxCollider>();
+        _SwordCollider.enabled = false;//コライダーはOFF
+
+        _NextActionTime = _CombAttackGraceTime;
+
+        _IsGaraceTime = false;
+        _IsAvoid = false;
     }
     void Update()
     {
+        if (_IsAvoid)
+        {
+            Avoidance();
+            return;
+        }
+
         PlayerWalk();//プレイヤー移動関数呼び出し
         PlayerAttackAnimation();//Zキーを押した時にアニメーションをさせる
-
+        PlayerAvoidance();
     }
 
-    
 
+    float CalcMoveRatio()
+    {
+        var distCovered = (Time.deltaTime - _StartTime) * 5;
+        return distCovered / JourneyLength;
+    }
 
 
     private void PlayerWalk()
@@ -49,9 +99,17 @@ public class Player_Controll : MonoBehaviour
         float dx = Input.GetAxis("Horizontal") * Time.deltaTime * PlayerWlakSpeed;
         float dz = Input.GetAxis("Vertical") * Time.deltaTime * PlayerWlakSpeed;
 
-        transform.position = new Vector3(
+        bool IsAttack = PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack");
+
+        //攻撃時に動かさない
+        if (!IsAttack)
+        {
+            transform.position = new Vector3(
             transform.position.x + dx, 0.5f, transform.position.z + dz
         );
+        }
+        
+
         Vector3 diff = transform.position - latestPosition;   //現在居る位置からどの方向に進んだか
         latestPosition = transform.position;
         diff.y = 0;//y方向は無視する
@@ -64,29 +122,136 @@ public class Player_Controll : MonoBehaviour
         {
             PlayerAttackAnimator.SetBool("Run", false);
         }
-    }
-    public void PlayerAttackAnimation()
-    {
-        if(Input.GetKeyDown(KeyCode.Z))
-        {
-            //攻撃アニメーション再生
-            PlayerAttackAnimator.SetTrigger("Attack");
-        }
-    }
-    public void _AddDamege(int _Damege)
-    {//使わないかも
+
+        
+
+
+
     }
 
-    //---------------------------------------------------------------
-    //追加 AnimationEvent 攻撃コライダーの有効化と無効化（ジョ）
-    public void StartAttack()
+    /// <summary>
+    /// 回避
+    /// </summary>
+    private void PlayerAvoidance()
     {
-        attackArea.enabled = true;
+
+        if (Input.GetKeyDown(KeyCode.C) && !_IsAvoid)
+        {
+            
+            PlayerAttackAnimator.SetTrigger("Avoid");
+            
+            _IsAvoid = true;
+            AvoidPos_Start = transform.position;
+            AvoidPos_End =transform.position + transform.forward * _AvoidanceValue;
+        }
+
     }
-    public void EndAttack()
+
+    private void Avoidance()
     {
-        attackArea.enabled = false;
+        transform.position = Vector3.Lerp(transform.position, AvoidPos_End, CalcMoveRatio());
+
+        if (PlayerAttackAnimator.IsInTransition(0))//遷移中
+        {
+            return;
+        }
+
+        if (!PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("Avoidance"))
+        {
+            _IsAvoid = false;
+        }
+
     }
-    //---------------------------------------------------------------
+
+    public void PlayerAttackAnimation()
+    {
+
+        //時間を計算
+        _NextActionTime += Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Z)&&!_SwordCollider.enabled)
+        {
+
+            //猶予時間の終わり
+            _IsGaraceTime = false;
+
+            //連続攻撃
+            if (_NextActionTime <= _CombAttackGraceTime)//猶予時間内にイベントが発生時
+            {
+                PlayerAttackAnimator.SetFloat("AttackSpeed", 1);
+
+                ++_AttackType;
+
+                if (_AttackType >= 3)
+                {
+                    _AttackType = 0;
+                }
+            }
+            else
+            {
+                //猶予時間を超えるとType１のアニメーションにする
+                _AttackType = 0;
+
+            }
+
+            //AnimetorParameterに反映
+            PlayerAttackAnimator.SetFloat("AttackType", _AttackType);
+
+            //アニメーションを最初から再生
+            PlayerAttackAnimator.Play("Attack",0,0);
+
+            //攻撃アニメーション再生
+            PlayerAttackAnimator.SetBool("IsAttack",true);
+
+            StartCoroutine(AttackColliderTime());
+        }
+
+
+        if(_NextActionTime >= _CombAttackGraceTime)//猶予時間を超えたら
+        {
+            //アニメーションの再生
+            PlayerAttackAnimator.SetFloat("AttackSpeed", 1);
+
+            //アニメーションの遷移
+            PlayerAttackAnimator.SetBool("IsAttack", false);
+
+            //猶予時間の終わり
+            _IsGaraceTime = false;
+        }
+
+        if(_IsGaraceTime)//猶予時間であるとき
+        {
+            //アニメーションの停止
+            PlayerAttackAnimator.SetFloat("AttackSpeed", 0);
+        }
+
+    }
+    public void _AddDamege(int _Damege)
+    {
+
+
+    }
+
+    IEnumerator AttackColliderTime()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        //当たり判定をONにする
+        _SwordCollider.enabled = true;
+
+        yield return new WaitForSeconds(0.5f);//攻撃終了
+
+        //0.5秒にColliderをOFF
+        _SwordCollider.enabled = false;
+
+        //猶予時間のリセット
+        _NextActionTime = 0;
+
+        //猶予時間の始まり
+        _IsGaraceTime = true;
+
+        yield break;
+    }
+
 }
 
