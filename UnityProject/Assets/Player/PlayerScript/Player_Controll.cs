aa02f3100b2448894ff0401c80c3c5276d761e1d
MiyabaraYouta <miyayou0527@gmail.com>
2022-02-26 13:40:33 +0900

    Playerのレーン移動を完成

--- UnityProject/Assets/Player/PlayerScript/Player_Controll.cs
+++ UnityProject/Assets/Player/PlayerScript/Player_Controll.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Player_Controll : MonoBehaviour,IPlayerDamege
+public class Player_Controll : MonoBehaviour, IPlayerDamege
 {
     [SerializeField, Tooltip("RigidBody")]
     private Rigidbody _RigidBody = null;
@@ -38,6 +38,12 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
     [SerializeField, Tooltip("回避の大きさ")]
     private float _AvoidanceValue = 50.0f;
 
+    [SerializeField, Tooltip("回避のリロード時間")]
+    private float AvoidReLoadTime = 3.0f;
+
+    [SerializeField, Tooltip("レーンの座標")]
+    private List<float> _LanePosList;
+
     [SerializeField, Tooltip("最大ライフ")]
     private float _MAXHP = 100;
 
@@ -45,7 +51,7 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
     private Animator PlayerAttackAnimator;
     public Collider _SwordCollider;
     private GunControll _GunControll;
-    
+
     private int _AttackType = 0;
     private float _NextActionTime;
 
@@ -53,17 +59,19 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
     private bool _IsGaraceTime = false;
     private bool _IsAvoid = false;
     private bool _IsAttack = false;
+    private bool _IsLaneChamge = false;
 
     public float SmoothTime = 2f;
     public float Speed = 1f;
     public float JourneyLength = 10f;
     private float _StartTime = 0;
     private float _NowHP;
-
-
+    private int _NowLane;
+    private float Input_tmp = 0;
+    private float InputZ_tmp = 0;
     private Vector3 AvoidPos_Start = new Vector3();
     private Vector3 AvoidPos_End = new Vector3();
-
+    private float AvoidAngle = 0; 
 
     public static float AttackBuff = 1;
 
@@ -86,6 +94,7 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
         _NowHP = _MAXHP;
         AttackBuff = DataManager.Instance._PlayerAttackBuff;
 
+        _NowLane = 0;//初期
 
         //武器の当たり判定の設定
         _SwordCollider = _SwordObject.GetComponent<BoxCollider>();
@@ -93,13 +102,14 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
 
         _NextActionTime = _CombAttackGraceTime;
 
+        Input_tmp = 1;
         _IsGaraceTime = false;
         _IsAvoid = false;
         _IsAttack = false;
     }
     void Update()
     {
-        
+
 
         if (_IsAvoid)
         {
@@ -111,6 +121,8 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
         PlayerAttackAnimation();//Zキーを押した時にアニメーションをさせる
         PlayerAvoidance();
         PlayercolliderONOFF();
+        ChangeLaneMove();
+
 
         //Playerの攻撃力反映
         AttackBuff = DataManager.Instance._PlayerAttackBuff;
@@ -125,7 +137,7 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
 
     private void PlayercolliderONOFF()
     {
-        if (!PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("OnDamage")||!PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("Avoidance"))
+        if (!PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("OnDamage") || !PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("Avoidance"))
         {
             _HitBase.enabled = true;
         }
@@ -137,40 +149,148 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
     {//プレイヤーの移動関数
         float dx = Input.GetAxis("Horizontal") * Time.deltaTime * PlayerWlakSpeed;
         float dz = Input.GetAxis("Vertical") * Time.deltaTime * PlayerWlakSpeed;
-
+        
         bool IsAttack = PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack");
+        bool IschangeLane = PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("ChangeLane");
+
 
         //攻撃時に動かさない
-        if (!IsAttack)
+        if (!IsAttack&&!IschangeLane)
         {
             transform.position = new Vector3(
-            transform.position.x + dx, 0.5f, transform.position.z + dz
-        );
+            transform.position.x + dx, 0.5f, _LanePosList[_NowLane]) ;
         }
-        
+
+        //向きデータを保存
+        if (dx != 0)
+        {
+            Input_tmp = dx;
+        }
+
 
         Vector3 diff = transform.position - latestPosition;   //現在居る位置からどの方向に進んだか
         latestPosition = transform.position;
         diff.y = 0;//y方向は無視する
+        diff.z = 0;
         if (diff.magnitude > 0.01f)
         {
-            if (!_IsAvoid)
+            if (!_IsAvoid||!_IsLaneChamge)
             {
                 transform.rotation = Quaternion.LookRotation(diff); //プレイヤーの向き変更
                 PlayerAttackAnimator.SetBool("Run", true);
             }
-            
+
         }
         else
         {
             PlayerAttackAnimator.SetBool("Run", false);
         }
 
+        //レーン移動
+        if (!_IsLaneChamge)
+        {
+            ChangeLaneFun(dz);
+        }
 
+
+    }
+
+    /// <summary>
+    /// レーン移動
+    /// </summary>
+    /// <param name="inp">入力情報</param>
+    void ChangeLaneFun(float inp)
+    {
+        if (inp != 0)
+        {
+            if (_NowLane == 0)
+            {
+                if (inp < 0)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                if (inp > 0)
+                {
+                    return;
+                }
+            }
+            //アニメーション再生
+            PlayerAttackAnimator.SetTrigger("ChangeLane");
+
+            InputZ_tmp = inp;
+        }
+
+
+       
+
+    }
+
+    /// <summary>
+    /// レーン移動するときの動き
+    /// </summary>
+    void ChangeLaneMove()
+    {
+        //2点間の距離を代入する
+        float distance = _LanePosList[1] - _LanePosList[0];
+
+        //現在の位置
+        float present_Location = (Time.time*0.01f) / distance;
+
+        if (_IsLaneChamge)
+        {
+            //オブジェクトの移動
+            this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y, _LanePosList[_NowLane]), 0.08f);
+
+            //レーン移動の終了
+            if (present_Location >= 1)
+            {
+                //_IsLaneChamge = false;
+            }
+
+        }
 
 
 
     }
+
+    public void ChangeLaneAnime_Start()
+    {
+        _IsLaneChamge = true;
+
+        if (InputZ_tmp > 0)//加算
+        {
+            _NowLane++;
+            //上限保護
+            if (_NowLane >= _LanePosList.Count)
+            {
+                _NowLane = _LanePosList.Count - 1;
+                return;
+            }
+        }
+        else if (InputZ_tmp < 0)//減算
+        {
+            _NowLane--;
+            //下限保護
+            if (_NowLane < 0.00f)
+            {
+                _NowLane = 0;
+                return;
+            }
+        }
+
+
+    }
+    public void ChangeLaneAnime_End()
+    {
+        _IsLaneChamge = false;
+        PlayerAttackAnimator.ResetTrigger("ChangeLane");
+
+    }
+
+
 
     /// <summary>
     /// 回避
@@ -185,12 +305,22 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
 
         if (Input.GetKeyDown(KeyCode.C) && !_IsAvoid)
         {
-
+            //アニメーション再生
             PlayerAttackAnimator.SetTrigger("Avoid");
 
             _IsAvoid = true;
             AvoidPos_Start = transform.position;
-            AvoidPos_End = transform.position + transform.forward * _AvoidanceValue;
+
+            if (Input_tmp > 0)
+            {
+                AvoidPos_End = transform.position + new Vector3(_AvoidanceValue, 0, 0);//普通の回
+            }
+            else
+            {
+                AvoidPos_End = transform.position + new Vector3(-_AvoidanceValue, 0, 0);//普通の回
+            }
+            StartCoroutine(AvoidReLoad_IE());
+
             return true;
         }
 
@@ -199,7 +329,7 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
 
     private void Avoidance()
     {
-        transform.position = Vector3.Lerp(transform.position, AvoidPos_End, CalcMoveRatio());
+
 
         if (PlayerAttackAnimator.IsInTransition(0))//遷移中
         {
@@ -211,12 +341,21 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
             _IsAvoid = false;
         }
 
+        if (_IsAvoid)
+        {
+            transform.position = Vector3.Lerp(transform.position, AvoidPos_End, CalcMoveRatio());
+        }
+        else
+        {
+            _IsLaneChamge = false;
+        }
+
     }
 
     public void PlayerAttackAnimation()
     {
 
-        
+
         if (PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("run") || PlayerAttackAnimator.GetCurrentAnimatorStateInfo(0).IsName("Stand"))
         {
             //剣の攻撃
@@ -225,7 +364,7 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
             //銃での攻撃
             _GunControll.GunAttack();
         }
-       
+
 
 
     }
@@ -250,7 +389,7 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
         _IsGaraceTime = true;
         StartCoroutine(AttackColliderTime());
 
-        PlayerAttackAnimator.SetBool("IsAttack", false);
+        PlayerAttackAnimator.ResetTrigger("Attack");
     }
 
     //剣での攻撃
@@ -259,10 +398,11 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
         if (Input.GetKeyDown(KeyCode.Z) && !_SwordCollider.enabled)
         {
             _SwordCollider.enabled = true;
-          
+
             PlayerAttackAnimator.SetTrigger("Attack");
-            
+
             PlayerAttackAnimator.Play("Attack", 0, 0);
+            Debug.Log("SwordAttack!!");
             return true;
         }
         return false;
@@ -301,6 +441,16 @@ public class Player_Controll : MonoBehaviour,IPlayerDamege
         _AttackType = 0;
         PlayerAttackAnimator.SetFloat("AttackType", _AttackType);
         yield break;
+    }
+
+    IEnumerator AvoidReLoad_IE()
+    {
+        yield return new WaitForSeconds(AvoidReLoadTime);
+        _IsAvoid = false;
+
+        yield break;
+
+
     }
 
 }
