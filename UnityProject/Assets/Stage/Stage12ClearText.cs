ccfac261aff42cccbbf86beadb16b34f4a2cff32
MSI\kazus <21ci0521@jec.ac.jp>
2022-02-27 17:43:07 +0900

    Enemy1,2Boss

---
+++ UnityProject/Assets/Stage/Stage12ClearText.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stage12ClearText : MonoBehaviour
+{
+    private string maxDispStr = ""; //表示させたい内容の文字列
+    private string nowDispStr = ""; //実際に画面に表示させる用の文字列
+    [SerializeField, Tooltip("文字のスピード")]
+    private float nowDispCount = 0.0f; //現在何文字目まで表示するかのカウンター
+    public Text text = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxDispStr = "クリアしたよ";
+        nowDispCount += Time.deltaTime / 0.05f;  //文字表示速度
+        nowDispStr = maxDispStr.Substring(0, Mathf.Min((int)nowDispCount, maxDispStr.Length));
+        text.text = nowDispStr;
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            FadeManager.Instance.LoadScene("shop", 1.0f);
+        }
+    }
+}
